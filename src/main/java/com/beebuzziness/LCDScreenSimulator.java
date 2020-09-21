f7c9c88b09f40e7943f8329a56cf01ac3012669c
Mathieu <mathieu@sogilis.com>
2020-09-21 10:09:08 +0200

    Init maven project

---
+++ src/main/java/com/beebuzziness/LCDScreenSimulator.java
@@ -0,0 +1,9 @@
+package com.beebuzziness;
+
+public class LCDScreenSimulator {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ src/test/java/com/beebuzziness/LCDScreenSimulatorTest.java
@@ -0,0 +1,14 @@
+package com.beebuzziness;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class LCDScreenSimulatorTest {
+
+	@Test
+	public void MainTest() {
+		assertEquals(true, true);
+	}
+	
+}
